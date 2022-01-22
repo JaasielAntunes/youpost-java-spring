8a77c30a920c61f69734b65db3055f2857f9d73e
Jaasiel Antunes <jasiel.araujo098@gmail.com>
2022-01-22 18:27:31 -0300

    Project created

---
+++ src/main/java/com/antunes/youpost/YoupostApplication.java
@@ -0,0 +1,13 @@
+package com.antunes.youpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoupostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoupostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antunes/youpost/YoupostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antunes.youpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoupostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
