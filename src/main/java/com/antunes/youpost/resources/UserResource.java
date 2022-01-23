c6d91deebcddaa3c8e612316fcb82045ae7cd960
Jaasiel Antunes <jasiel.araujo098@gmail.com>
2022-01-23 12:18:30 -0300

    Standard DTO

--- src/main/java/com/antunes/youpost/resources/UserResource.java
+++ src/main/java/com/antunes/youpost/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.antunes.youpost.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.antunes.youpost.domain.User;
+import com.antunes.youpost.dto.UserDTO;
 import com.antunes.youpost.services.UserService;
 
 @RestController
@@ -19,9 +21,10 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 		
 	}
 
