0a96e56d5411c8d4f6428fb3110c41d953528c19
Jaasiel Antunes <jasiel.araujo098@gmail.com>
2022-01-23 13:16:51 -0300

    Getting a user by id

--- src/main/java/com/antunes/youpost/services/UserService.java
+++ src/main/java/com/antunes/youpost/services/UserService.java
@@ -1,12 +1,14 @@
 package com.antunes.youpost.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.antunes.youpost.domain.User;
 import com.antunes.youpost.repository.UserRepository;
+import com.antunes.youpost.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
 	}
 
 }
