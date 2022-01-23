076242585408a374593dc08bb4b62f780cc0601b
Jaasiel Antunes <jasiel.araujo098@gmail.com>
2022-01-23 13:49:35 -0300

    User deletion

--- src/main/java/com/antunes/youpost/resources/UserResource.java
+++ src/main/java/com/antunes/youpost/resources/UserResource.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -33,7 +34,7 @@ public class UserResource {
 		
 	}
 	
-	@RequestMapping(value = "/{id}")
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);	
 		return ResponseEntity.ok().body(new UserDTO(obj));
@@ -46,6 +47,13 @@ public class UserResource {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<UserDTO> deleteById(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+		
 	}
 
 }
