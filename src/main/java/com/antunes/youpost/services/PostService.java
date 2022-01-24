551498a34407fbb7e249133407e5b7ffa68ef35f
Jaasiel Antunes <jasiel.araujo098@gmail.com>
2022-01-24 14:39:35 -0300

    Getting a post by id

---
+++ src/main/java/com/antunes/youpost/services/PostService.java
@@ -0,0 +1,23 @@
+package com.antunes.youpost.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.antunes.youpost.domain.Post;
+import com.antunes.youpost.repository.PostRepository;
+import com.antunes.youpost.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+	
+}
